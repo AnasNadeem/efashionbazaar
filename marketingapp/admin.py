2473e976e24a320c7d2a210ff46a024fdd43c6a5
Anas Nadeem <anas5678go@gmail.com>
2023-05-31 17:41:02 +0530

    Marketingapp models added

--- marketingapp/admin.py
+++ marketingapp/admin.py
@@ -1,3 +1,70 @@
 from django.contrib import admin
+from marketingapp.models import (
+    AboutUs,
+    ContactUs,
+    CouponCode,
+    FAQ,
+    Newsletter,
+    PrivacyPolicy,
+    SocialMedia,
+    TermsAndCondition,
+)
 
-# Register your models here.
+
+class TimeBaseModelAdmin(admin.ModelAdmin):
+    list_display = ('is_active', 'created', 'updated')
+    list_filter = ('is_active', 'created', 'updated')
+    readonly_fields = ('created', 'updated')
+    list_editable = ('is_active',)
+
+
+class AboutUsAdmin(TimeBaseModelAdmin):
+    list_display = ('title', 'description',) + TimeBaseModelAdmin.list_display
+    search_fields = ('title',)
+
+
+class ContactUsAdmin(TimeBaseModelAdmin):
+    list_display = ('name', 'email', 'phone_number', 'subject',) + TimeBaseModelAdmin.list_display
+    search_fields = ('name', 'email', 'phone_number', 'subject', 'message',)
+
+
+class CouponCodeAdmin(TimeBaseModelAdmin):
+    list_display = ('code', 'discount',) + TimeBaseModelAdmin.list_display
+    search_fields = ('code',)
+
+
+class FAQAdmin(TimeBaseModelAdmin):
+    list_display = ('question',) + TimeBaseModelAdmin.list_display
+    search_fields = ('question',)
+    ordering = ('-created',)
+
+
+class NewsletterAdmin(TimeBaseModelAdmin):
+    list_display = ('email', 'phone_number',) + TimeBaseModelAdmin.list_display
+    search_fields = ('email', 'phone_number',)
+    ordering = ('-created',)
+
+
+class PrivacyPolicyAdmin(TimeBaseModelAdmin):
+    list_display = ('title', 'description',) + TimeBaseModelAdmin.list_display
+    search_fields = ('title',)
+
+
+class SocialMediaAdmin(TimeBaseModelAdmin):
+    list_display = ('name', 'url',) + TimeBaseModelAdmin.list_display
+    search_fields = ('name', 'url',)
+
+
+class TermsAndConditionAdmin(TimeBaseModelAdmin):
+    list_display = ('title', 'description',) + TimeBaseModelAdmin.list_display
+    search_fields = ('title',)
+
+
+admin.site.register(AboutUs, AboutUsAdmin)
+admin.site.register(ContactUs, ContactUsAdmin)
+admin.site.register(CouponCode, CouponCodeAdmin)
+admin.site.register(FAQ, FAQAdmin)
+admin.site.register(Newsletter, NewsletterAdmin)
+admin.site.register(PrivacyPolicy, PrivacyPolicyAdmin)
+admin.site.register(SocialMedia, SocialMediaAdmin)
+admin.site.register(TermsAndCondition, TermsAndConditionAdmin)
--- marketingapp/models.py
+++ marketingapp/models.py
@@ -1,3 +1,130 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
-# Create your models here.
+from utils.models_base import TimeBaseModel
+
+
+class AboutUs(TimeBaseModel):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'About Us'
+        verbose_name_plural = 'About Us'
+        ordering = ['-created']
+
+
+class ContactUs(TimeBaseModel):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    subject = models.CharField(max_length=200)
+    message = models.TextField()
+    phone_number = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.name
+
+    def clean(self):
+        if not self.phone_number.startswith('+91'):
+            raise ValidationError('Phone number should be Indian')
+
+        if len(self.phone_number) != 13:
+            raise ValidationError('Phone number should be 10 digits')
+
+        if not self.phone_number[3:].isdigit():
+            raise ValidationError('Phone number should be digits')
+
+    class Meta:
+        verbose_name = 'Contact Us'
+        verbose_name_plural = 'Contact Us'
+        ordering = ['-created']
+
+
+class CouponCode(TimeBaseModel):
+    code = models.CharField(max_length=15)
+    discount = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.code
+
+    class Meta:
+        verbose_name = 'Coupon Code'
+        verbose_name_plural = 'Coupon Codes'
+        ordering = ['-created']
+        unique_together = ('code', 'is_active')
+
+
+class FAQ(TimeBaseModel):
+    question = models.CharField(max_length=200)
+    answer = models.TextField()
+
+    def __str__(self):
+        return self.question
+
+    class Meta:
+        verbose_name = 'FAQ'
+        verbose_name_plural = 'FAQs'
+        ordering = ['-created']
+
+
+class Newsletter(TimeBaseModel):
+    email = models.EmailField(max_length=100)
+    phone_number = models.CharField(max_length=15, blank=True)
+
+    def __str__(self):
+        return f'{self.email} - {self.phone_number}'
+
+    class Meta:
+        verbose_name = 'Newsletter'
+        verbose_name_plural = 'Newsletters'
+        ordering = ['-created']
+
+
+class PrivacyPolicy(TimeBaseModel):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Privacy Policy'
+        verbose_name_plural = 'Privacy Policies'
+        ordering = ['-created']
+
+
+class SocialMedia(TimeBaseModel):
+    SOCIAL_MEDIA_CHOICES = (
+        ('facebook', 'Facebook'),
+        ('twitter', 'Twitter'),
+        ('instagram', 'Instagram'),
+        ('youtube', 'Youtube'),
+    )
+
+    name = models.CharField(max_length=50, choices=SOCIAL_MEDIA_CHOICES)
+    url = models.URLField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Social Media'
+        verbose_name_plural = 'Social Media'
+        ordering = ['name']
+        unique_together = ('name', 'url')
+
+
+class TermsAndCondition(TimeBaseModel):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Terms And Condition'
+        verbose_name_plural = 'Terms And Conditions'
+        ordering = ['-created']
